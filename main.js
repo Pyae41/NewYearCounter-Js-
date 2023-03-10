cba3ee61a5f6d76d3760c825f605e8757a185fc8
Pyae Phyo Zan <94029981+Pyae41@users.noreply.github.com>
2023-03-10 22:05:03 +0630

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const secondsCl = document.getElementById("seconds");
 
 
 
-const newYear =`1 Jan {new Date().getFullYear()}`;
+const newYear =`1 Jan ${new Date().getFullYear()}`;
 
 function countDown() {
     const newYearsDate = new Date(newYear);
